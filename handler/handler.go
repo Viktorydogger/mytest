19c61f736fb9f66276bac8b8a90b0e8a77d49512
Viktorydogger <valcevav@gmail.com>
2022-11-15 19:30:59 +0300

    пусть хоть что-нибудь заработает

---
+++ handler/handler.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
+	"gitlab.com/idoko/bucketeer/db"
+	"net/http"
+)
+
+var dbInstance db.Database
+
+func NewHandler(db db.Database) http.Handler {
+	router := chi.NewRouter()
+	dbInstance = db
+	router.MethodNotAllowed(methodNotAllowedHandler)
+	router.NotFound(notFoundHandler)
+	router.Route("/items", items)
+	return router
+}
+func methodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(405)
+	render.Render(w, r, ErrMethodNotAllowed)
+}
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(400)
+	render.Render(w, r, ErrNotFound)
+}
